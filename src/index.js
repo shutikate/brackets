4633817f159c20a5309add5a1d126fceeec0b712
Katsiaryna Shutsikava <shutikova.k@gmail.com>
2022-08-08 14:51:10 +0200

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const brackets = bracketsConfig.flat();
+  let arr = [];
+  for (let i = 0; i < str.length; i++) {
+    let bracketsIndex = brackets.indexOf(str[i]);
+    if (arr.length !== 0 && str[i] === brackets[arr[arr.length - 1]]) {
+      arr.pop();
+    } else if (bracketsIndex % 2 === 0) {
+      arr.push(bracketsIndex + 1);
+    } else {arr.push(bracketsIndex)} 
+  }
+  if (arr.length === 0) {
+    return true;
+  }
+  return false;
 }
+
